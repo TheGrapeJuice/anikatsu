1c8b1b780e8f646e6a2bfcf716587c06868cbe69
TheGrapeJuice <102010764+TheGrapeJuice@users.noreply.github.com>
2024-10-05 15:44:34 -0500

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://grapejuice.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://apimirurothing.vercel.app/"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
